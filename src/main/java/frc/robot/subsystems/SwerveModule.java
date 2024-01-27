4b642f24828857b85d440fc4cc8fc3ef993d0adc
Khalil-Burns <khalilburns@gmail.com>
2024-01-26 21:14:30 -0500

    addded comment to sweve module

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -197,6 +197,7 @@ public class SwerveModule {
    */
   private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop) {
     if (isOpenLoop) {
+      // double percentOutput = desiredState.speedMetersPerSecond / Config.Swerve.maxSpeed;
       double speed = desiredState.speedMetersPerSecond * desiredState.angle.minus(getAngle()).getCos();
       double percentOutput = speed / Config.Swerve.maxSpeed;
       driveMotor.set(percentOutput);
