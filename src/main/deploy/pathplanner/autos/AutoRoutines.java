ad42b873c7d586efa2d99344e181b50efc2dfb01
cooking-hambrops <ethan.soh360@gmail.com>
2024-01-27 13:22:39 -0500

    Co-authored-by: Khalil-Burns <Khalil-Burns@users.noreply.github.com>

--- src/main/deploy/pathplanner/autos/AutoRoutines.java
+++ src/main/deploy/pathplanner/autos/AutoRoutines.java
@@ -1,7 +1,8 @@
 public class AutoRoutines extends SubsystemBase {
     PathPlannerPath path1 = PathPlannerPath.fromPathFile("2mStraightLines");
     PathPlannerPath path2 = PathPlannerPath.fromPathFile("Diagonal45Degrees");
-
+    PathPlannerPath path3 = PathPlannerPath.fromPathFile("Path 3");
+    PathPlannerPath path4 = PathPlannerPath.fromPathFile("Auto path drive out of starting zone and score in amp");
     public AutoRoutines() {
         
     }
@@ -15,6 +16,8 @@ public class AutoRoutines extends SubsystemBase {
                 return AutoBuilder.followPathWithEvents(path1);
             case 2:
                 return AutoBuilder.followPathWithEvents(path2);
+            case 3:
+                return AutoBuilder.followPathWithEvents(path3);
         }
     }
 }
