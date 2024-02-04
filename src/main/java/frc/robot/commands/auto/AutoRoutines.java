5719ebd65cfec2efe162cb7c9d3e07818ed4b90e
ErikCald <erikcaell@gmail.com>
2024-02-03 21:23:13 -0500

    Add placeholder named commands

--- src/main/java/frc/robot/commands/auto/AutoRoutines.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines.java
@@ -1,13 +1,20 @@
 package frc.robot.commands.auto;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Config.PhotonConfig.PhotonPositions;
+import frc.robot.subsystems.PhotonSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class AutoRoutines extends SubsystemBase {
@@ -16,6 +23,30 @@ public class AutoRoutines extends SubsystemBase {
 
     public AutoRoutines() {
         
+    }
+
+    public static Command visionScoreSpeakerCommand(PhotonPositions scorePos) {
+        return new ParallelDeadlineGroup(
+            new SequentialCommandGroup(
+                PhotonSubsystem.getInstance().getAprilTagCommand(scorePos),
+                new WaitUntilCommand(() -> {return true;}), // Wait for arm to be in position
+                new WaitUntilCommand(() -> {return true;}), // Wait for shooter to be spun up,
+                new WaitCommand(0.3) // Move intake for shoot command
+            ),
+            new WaitCommand(2), // Move arm to speaker center close setpoint
+            new WaitCommand(3) // Spin shooter up to speed 
+        );
+    }
+
+    public static void registerCommandsToPathplanner() {
+        // Vision Commands
+        NamedCommands.registerCommand("VisionScoreSpeakerCenter", visionScoreSpeakerCommand(PhotonPositions.MIDDLE_SPEAKER_BLUE));
+
+        // Intake and Arm Commands
+        NamedCommands.registerCommand("IntakeAndArm", new ParallelCommandGroup(
+            new WaitCommand(1), // Move arm to intake setpoint
+            new WaitCommand(1) // Intake game piece
+        ));
     }
 
     public Command getAutonomousCommand(int selectAuto) {
