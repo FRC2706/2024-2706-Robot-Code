a462b675b2eaec829d53cc7291d6dc94d2d90803
Khalil-Burns <khalilburns@gmail.com>
2024-02-17 11:06:20 -0500

    Updated robot.java id stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -57,7 +57,7 @@ public class Robot extends TimedRobot {
 
     switch (Config.getRobotId()) {
       case 0:
-        m_robotContainer = null; break; //competition
+        m_robotContainer = new NewRobotContainer(); break; //competition
         
       case 1:
         m_robotContainer = new ClutchContainer(); break; //simulation
