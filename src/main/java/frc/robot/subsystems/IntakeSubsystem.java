fb97fce20c900a3be4a671ced778d5f7abe77d25
emilycxliu <emily.chenxing.liu@gmail.com>
2024-02-17 13:31:43 -0500

    added sensor

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,11 +4,16 @@ import com.revrobotics.CANSparkMax;
 import frc.robot.Config;
 import com.revrobotics.SparkPIDController;
 import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 
 public class IntakeSubsystem extends SubsystemBase {
+
+    private DigitalInput sensor7; //DigitInput instance
 
     private static final IntakeSubsystem INSTANCE_INTAKE = new IntakeSubsystem();
 
