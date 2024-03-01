a597b9b47da0891d9240f7c653c3bfe9b2e05ee2
ErikCald <erikcaell@gmail.com>
2024-02-29 22:32:31 -0500

    Add missing import to BeetleContainer

--- src/main/java/frc/robot/robotcontainers/BeetleContainer.java
+++ src/main/java/frc/robot/robotcontainers/BeetleContainer.java
@@ -4,14 +4,16 @@
 
 package frc.robot.robotcontainers;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Robot;
-import frc.robot.commands.ClimberRPM;
 import frc.robot.commands.ArcadeDrive;
 import frc.robot.commands.BlingCommand;
 import frc.robot.commands.BlingCommand.BlingColour;
+import frc.robot.commands.ClimberRPM;
 import frc.robot.subsystems.DiffTalonSubsystem;
 
 /**
