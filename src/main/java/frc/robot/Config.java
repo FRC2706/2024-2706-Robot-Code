5a894d833c249b9a4f33da902a869c8a25fe258a
ErikCald <erikcaell@gmail.com>
2024-03-02 04:23:39 -0500

    Start with simple shooter/intake

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -136,7 +136,7 @@ public final class Config {
 
 
   public static final boolean swerveTuning = true; //tune swerve? Turn this to false for competition
-  public static final boolean disableStateBasedProgramming = false; // True to disable state based programming and use only simple commands
+  public static final boolean disableStateBasedProgramming = true; // True to disable state based programming and use only simple commands
 
   public static int ANALOG_SELECTOR_PORT = robotSpecific(3, -1, -1, 0);
 
