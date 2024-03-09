82d283b6bdf1cc964ec5cd01f764bfe03f6b59d0
weizhang-2706 <wei2001ca@yahoo.com>
2024-03-09 18:04:28 -0500

    add a safe button for climber

--- src/main/java/frc/robot/robotcontainers/NewRobotContainer.java
+++ src/main/java/frc/robot/robotcontainers/NewRobotContainer.java
@@ -151,7 +151,7 @@ public class NewRobotContainer extends RobotContainer {
     operator.a().onTrue(new SetArm(()->ArmSetPoints.INTAKE.angleDeg)); // Pickup
     operator.x().onTrue(new SetArm(()->ArmSetPoints.SPEAKER_KICKBOT_SHOT.angleDeg));
     // Climber
-    operator.leftTrigger(0.35).whileTrue(new ClimberRPM(()-> MathUtil.applyDeadband(operator.getLeftTriggerAxis(), 0.35) * 0.5));
+    operator.leftTrigger(0.35).and(operator.back()).whileTrue(new ClimberRPM(()-> MathUtil.applyDeadband(operator.getLeftTriggerAxis(), 0.35) * 0.5));
 
     // Eject the note from the front with start
     operator.start()
